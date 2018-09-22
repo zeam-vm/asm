56afd2780e9280bf99410e19dd8d392dce4cabdb
zacky1972 <zacky1972@gmail.com>
2018-09-22 14:19:45 +0900

    revise scope of max_int and min_int

--- lib/asm.ex
+++ lib/asm.ex
@@ -11,7 +11,9 @@ defmodule Asm do
   is_int64(value) returns true if the value is an integer, equals or is less than max_int and equals or is greater than min_int.
   """
   defmacro is_int64(value) do
-    quote do: is_integer(unquote(value)) and unquote(value) <= unquote(@max_int) and unquote(value) >= unquote(@min_int)
+    max_int = @max_int
+    min_int = @min_int
+    quote do: is_integer(unquote(value)) and unquote(value) <= unquote(max_int) and unquote(value) >= unquote(min_int)
   end
 
   def dummy(a), do: a
--- test/asm_test.exs
+++ test/asm_test.exs
@@ -1,16 +1,17 @@
+defmodule Foo do
+  require Asm
+  import Asm
+  def add(a, b) when is_int64(a) and is_int64(b) do
+    a + b
+  end
+end
+
 defmodule AsmTest do
   use ExUnit.Case
   doctest Asm
-
-  defmodule Foo do
-  	require Asm
-  	import Asm
-  	def add(a, b) when is_int64(a) and is_int64(b) do
-  		a + b
-  	end
-  end
 
   test "add" do
   	assert Foo.add(1, 2) == 3
   end
 end
+
