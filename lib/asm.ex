e2fcbdeed2e01ca3c0f66cabd4303cdd83637e3e
zacky1972 <zacky1972@gmail.com>
2018-09-22 12:00:36 +0900

    create is_int64

--- lib/asm.ex
+++ lib/asm.ex
@@ -6,4 +6,11 @@ defmodule Asm do
   @max_int 9_223_372_036_854_775_807
   @min_int -9_223_372_036_854_775_808
 
+
+  @doc """
+  is_int64(value) returns true if the value is an integer, equals or is less than max_int and equals or is greater than min_int.
+  """
+  defmacro is_int64(value) do
+    quote do: is_integer(unquote(value)) and unquote(value) <= unquote(@max_int) and unquote(value) >= unquote(@min_int)
+  end
 end
--- test/asm_test.exs
+++ test/asm_test.exs
@@ -1,4 +1,16 @@
 defmodule AsmTest do
   use ExUnit.Case
   doctest Asm
+
+  defmodule Foo do
+  	require Asm
+  	import Asm
+  	def add(a, b) when is_int64(a) and is_int64(b) do
+  		a + b
+  	end
+  end
+
+  test "add" do
+  	assert Foo.add(1, 2) == 3
+  end
 end
