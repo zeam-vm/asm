8f009ffced9913081c071231bcddd17640d2263e
zacky1972 <zacky1972@gmail.com>
2018-09-22 14:24:18 +0900

    revise scope of max_int and min_int

--- lib/asm.ex
+++ lib/asm.ex
@@ -3,17 +3,11 @@ defmodule Asm do
   Documentation for Asm.
   """
 
-  @max_int 9_223_372_036_854_775_807
-  @min_int -9_223_372_036_854_775_808
-
-
   @doc """
   is_int64(value) returns true if the value is an integer, equals or is less than max_int and equals or is greater than min_int.
   """
   defmacro is_int64(value) do
-    max_int = @max_int
-    min_int = @min_int
-    quote do: is_integer(unquote(value)) and unquote(value) <= unquote(max_int) and unquote(value) >= unquote(min_int)
+    quote do: is_integer(unquote(value)) and unquote(value) <= unquote(9_223_372_036_854_775_807) and unquote(value) >= unquote(-9_223_372_036_854_775_808)
   end
 
   def dummy(a), do: a
